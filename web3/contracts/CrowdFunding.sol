f28e09a4d0330696a9c762d7baef6431ebbf719c
Booze33 <tebetisloh@gmail.com>
2024-03-03 23:05:55 +0100

    Setup smart contract

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -37,25 +37,25 @@ contract CrowdFunding {
     }
 
     function donateToCampaign(uint256 _id) public payable {
-      uint256 amount - msg.value;
+      uint256 amount = msg.value;
 
-      Campaign storage campaign = campaign[_id];
+      Campaign storage campaign = campaigns[_id];
 
       campaign.donators.push(msg.sender);
       campaign.donations.push(amount);
 
-      (bool sent) = payable(campaign.owner).call{value: amount}("");
+      (bool sent,) = payable(campaign.owner).call{value: amount}("");
 
       if(sent) {
         campaign.amountCollected = campaign.amountCollected + amount;
       }
     }
 
-    function getDonators(uint256 _id) view public return(address[] memory, uint256[] memory) {
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
       return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function getCampaigns() public view return (Campaign[] memory) {
+    function getCampaigns() public view returns (Campaign[] memory) {
       Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
       for(uint i = 0; i < numberOfCampaigns; i++) {
