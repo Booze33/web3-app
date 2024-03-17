4b11c48a8511e4026f4d997285e87f0fd684d779
Booze33 <tebetisloh@gmail.com>
2024-03-17 21:56:43 +0100

    Create app

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ethers } from 'ethers';
 
+import { useStateContext } from '../context';
 import { money } from '../assets';
 import { CustomButton, FormField } from '../components';
 import { checkIfImage } from '../utils'
@@ -9,7 +10,8 @@ import { checkIfImage } from '../utils'
 const CreateCampaign = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
-  const [form, setForm] = useSate({
+  const createCampaign = useStateContext();
+  const [form, setForm] = useState({
     name: '',
     title: '',
     description: '',
@@ -25,7 +27,17 @@ const CreateCampaign = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    
+    checkIfImage(form.image, async (exists) => {
+      if(exists) {
+        setIsLoading(true)
+        await createCampaign({ ...form, target:ethers.utils.parseUnits(form.target, 18)})
+        setIsLoading(false);
+        navigate('/');
+      } else {
+        alert('Provide valid image URL')
+        setForm({ ...form, image: ''})
+      }
+    })
   }
 
   return (
