2a88e49bf95983e6318eb79235d8713788a6ff47
Booze33 <tebetisloh@gmail.com>
2024-03-22 00:46:27 +0100

    Create app

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { UseNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from "uuid";
 import FundCard from './FundCard';
 import { loader } from '../assets';
 
@@ -7,27 +8,26 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
   const navigate = useNavigate();
 
   const handleNavigate = (campaign) => {
-    navigate(`/campaign-details/${campaign.title}`, { state: campaign });
+    navigate(`/campaign-details/${campaign.title}`, { state: campaign })
   }
-
-  return(
+  
+  return (
     <div>
-      <h1 calssName="font-epilogue font-semibold text-white text-left text-[18px]">{title} ({campaigns.length})</h1>
+      <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">{title} ({campaigns.length})</h1>
 
       <div className="flex flex-wrap mt-[20px] gap-[26px]">
         {isLoading && (
           <img src={loader} alt="loader" className="w-[100px] h-[100px] object-contain" />
         )}
 
-        {isLoading && campaigns.length === 0 && (
-          <p className="font-epilogue font-semibold text-[#818183] leading-[30px] text-[14px]">
-            You Have not created any Campaigns yet.
+        {!isLoading && campaigns.length === 0 && (
+          <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
+            You have not created any campigns yet
           </p>
         )}
 
-        {!isLoading && campaigns.length > 0 && campaigns.map ((campaign) => 
-        <FundCard
-          key={campaign.id}
+        {!isLoading && campaigns.length > 0 && campaigns.map((campaign) => <FundCard 
+          key={uuidv4()}
           {...campaign}
           handleClick={() => handleNavigate(campaign)}
         />)}
