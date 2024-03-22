741c4d4f8513df3456cf01f2778fb21b66b3ecb9
Booze33 <tebetisloh@gmail.com>
2024-03-23 00:05:47 +0100

    Create app

---
+++ client/vite.config.js
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), nodePolyfills(), splitVendorChunkPlugin()],
+  define: {
+    "process.env": {},
+  },
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+  }
+});
